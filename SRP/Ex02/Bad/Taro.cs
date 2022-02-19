894eaa8dc5fe7a05d28dc7c21516a323fd8da382
fukuda <kfukuda@student.42tokyo.jp>
2022-02-19 20:27:21 +0900

    [add]Ex02のBadを追加

---
+++ SRP/Ex02/Bad/Taro.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP.Ex02.Bad
+{
+    class Taro
+    {
+        public Taro(bool isSleepyTaro, string daughterName, bool isHungryDaughter, string juniorColleagueName, bool isHungryJuniorColleague, bool isTieredJuniorColleague)
+        {
+            IsSleepyTaro = isSleepyTaro;
+            DaughterName = daughterName;
+            IsHungryDaughter = isHungryDaughter;
+            JuniorColleagueName = juniorColleagueName;
+            IsHungryJuniorColleague = isHungryJuniorColleague;
+            IsTieredJuniorColleague = isTieredJuniorColleague;
+        }
+
+        public bool IsSleepyTaro { get; set; }
+        public string DaughterName { get; set; }
+        public bool IsHungryDaughter { get; set; }
+        public string JuniorColleagueName { get; set; }
+        public bool IsHungryJuniorColleague { get; set; }
+        public bool IsTieredJuniorColleague { get; set; }
+
+        private bool PlaysTrick()
+        {
+            return IsHungryDaughter;
+        }
+
+        private bool SkipsWork()
+        {
+            return IsHungryJuniorColleague && IsTieredJuniorColleague;
+        }
+
+        public void Warn(bool isDaughter)
+        {
+            string warnMessage;
+            if (isDaughter)
+            {
+                warnMessage = "こらっ、" + DaughterName + "ちゃん！！　だめだぞ！！！";
+                ShowWarnMessage(PlaysTrick(), warnMessage);
+            
+            }
+            else
+            {
+                warnMessage = JuniorColleagueName + "さん、僕も仕事をサボりたいよ。";
+                ShowWarnMessage(SkipsWork(), warnMessage);
+            }
+        }
+
+        private void ShowWarnMessage(bool DoneBadThing, string warnMessage)
+        {
+            if (DoneBadThing && IsSleepyTaro)
+            {
+                Console.WriteLine(warnMessage);
+            }
+            else
+            {
+                Console.WriteLine("特に叱る必要もないか...");
+            }
+        }
+    }
+}
